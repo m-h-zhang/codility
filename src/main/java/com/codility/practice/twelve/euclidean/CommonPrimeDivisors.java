baf0a01538f6cd0fc9da40eda4c826e2206306ec
michael.zhang <zhanghao27@hotmail.com>
2020-03-03 23:49:22 +1100

    fix the but

--- src/main/java/com/codility/practice/twelve/euclidean/CommonPrimeDivisors.java
+++ src/main/java/com/codility/practice/twelve/euclidean/CommonPrimeDivisors.java
@@ -1,62 +1,51 @@
 package com.codility.practice.twelve.euclidean;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class CommonPrimeDivisors {
 	
 	  public int solution(int[] A, int[] B) {
 		  
-		 int count = 0;
-		  
-		  for ( int i = 0; i < A.length; i++) {
-			   Set<Integer> aPrimes = getPrimeFactors(A[i]);
-			   Set<Integer> bPrimes = getPrimeFactors(B[i]);
-			   if ( aPrimes.equals(bPrimes)) {
-				   count++;
-			   }
-		  }
-		  
- 
-		  
-		  
-		   
-		   return count;
+		  int count = 0;
+	        for(int i=0;i<A.length;i++) {
+	            if(hasSamePrimeDivisors(A[i], B[i])){
+	                count++;    
+	            } 
+	        }
+	        return count;
 	      
 	   }
 	  
 	  
-	  public Set<Integer> getPrimeFactors(int N) {
-             
-		  Set<Integer> s  = new HashSet<>();
-		  int num  = N;
-		  if ( num % 2 == 0 ) {
-			  s.add(2);
-			  while ( num % 2 == 0) {
-				 
-				  num = num /2;
-			  }
-		  }
-		  
-		  for ( int i  = 3; i <= Math.sqrt( num); i = i + 2) {
-			  if ( num % i == 0) {
-				  s.add(i);
-				  while ( num % i == 0) {
-					  num = num / i;
-				  }
-			  }
-		  }
-		  if (num > 2) {
-			  s.add(num);
-		  }
-		  
-		  return s;
-	  }
-	  
-	   
+ 
+	  public boolean hasSamePrimeDivisors(int a, int b) {
+		    int gcdValue = gcd(a,b);
+	        int gcdA;
+	        int gcdB;
+	        while(a!=1) {
+	            gcdA = gcd(a,gcdValue);
+	            if(gcdA==1)
+	                break;
+	            a = a/gcdA;
+	        }
+	        if(a!=1)  {
+	            return false;
+	        }
+	        while(b!=1) {
+	            gcdB = gcd(b,gcdValue);
+	            if(gcdB==1)
+	                break;
+	            b = b/gcdB;
+	        }
+	        return b==1;        
+		}
 	  
 	 
-	 
+	   public int gcd(int a, int b) {
+		   		   
+		    if(a % b == 0) return b;
+		    return gcd(b,a%b);
+		}
+	  
+	  
 
 	  
 	  public static void main(String arg[]) {
